c7266374dcce2bcd64c7403e29c7c9aad7a0571d
Jake <jake.rozin@gmail.com>
2015-06-05 15:36:33 -0700

    adding some sugar for customization

--- gulp/generate.js
+++ gulp/generate.js
@@ -15,4 +15,20 @@ module.exports = function (gulp, gutil) {
     generate()(gulp, gutil).scaffold(cb);
   });
 
+  /** Copy existing components from core source for customization
+    * TODO for now, we copy files from bower_components
+    * TODO future: grab content from github when repo is public
+    **/
+  gulp.task('clone', function (cb) {
+    if (!gutil.env.path) {
+      console.log(gutil.colors.cyan('gulp clone - copy existing components for customization'));
+      console.log(gutil.colors.cyan('Please provide a source path to clone'));
+      console.log(gutil.colors.yellow('Usage:') + ' gulp clone --path <PATH_TO_CLONE>');
+      process.exit(1);
+    } else {
+      generate()(gulp, gutil).clone(cb);
+    }
+
+  });
+
 };
