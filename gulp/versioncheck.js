989404f5d48d64bb3fa376c86a132345ab644051
megha.meshram <megha.meshram@lithium.com>
2015-08-19 13:16:48 -0700

    LIA-43952 mocha tests for version check

--- gulp/versioncheck.js
+++ gulp/versioncheck.js
@@ -4,43 +4,54 @@ var request = require('request');
 var pluginUtils = require('../lib/plugin-utils');
 var supportedMinVersion = 15.7;
 
-
 module.exports = function (gulp, gutil) {
 
-    function process(server) {
+    function process(server, cb) {
         try {
             if (server.serverUrl() === undefined) {
                 var errMessage = 'A server URL is required in your configuration. ';
-                gutil.log(gutil.colors.red(errMessage));
-                throw(new Error(errMessage));
+                callbackOrThrowError(cb, errMessage);
             }
-            var versionCheckUrl = pluginUtils.urlBldr(server.serverUrl() + '/status/version').build();
+            var versionCheckUrl = pluginUtils.urlBldr(server.serverUrl() + 'status/version').build();
             request(versionCheckUrl, function (error, response, body) {
                 if (error || response.statusCode > 201) {
-                    gutil.log(gutil.colors.red(error.message));
-                    throw(error);
+                    callbackOrThrowError(cb, error.message);
                 } else {
                     var matches = body.match(/<body>(\d+(\.\d)*)/i);
                     if (matches) {
                         var versionOnServer = matches[1];
                         if (versionOnServer < supportedMinVersion) {
                             var errorMessage = "Supported minimum version on server is " + supportedMinVersion;
-                            gutil.log(gutil.colors.red(errorMessage));
-                            throw(new Error(errorMessage));
+                            callbackOrThrowError(cb, errorMessage);
                         }
+                    } else {
+                        callbackOrThrowError(cb, "Invalid version check response from server");
                     }
                 }
             });
         } catch (err) {
-            gutil.log(gutil.colors.red(err.message));
-            throw(err);
+            callbackOrThrowError(cb, err.message);
         }
-    };
+
+    }
+
+    function callbackOrThrowError(cb, errorMsg) {
+        pluginUtils.logError(gutil, errorMsg);
+        if (typeof cb !== 'undefined') {
+            cb(new Error(errorMsg), null);
+        } else {
+            throw new Error(errorMsg);
+        }
+    }
 
     gulp.task('version-check', function() {
         var server = require('../lib/server.js')(gulp, gutil);
         process(server);
     });
 
-
+    return {
+        process: function (server, cb) {
+            process(server, cb);
+        }
+    };
 };
--- test/lib/versioncheck-test.js
+++ test/lib/versioncheck-test.js
@@ -3,14 +3,17 @@
 var nock = require('nock');
 var chai = require('chai');
 var expect = chai.expect;
-var sinon = require('sinon');
 var path = require('path');
 var gutil = require('gulp-util');
-var apiHost = 'https://mycommunity.com:443';
+var apiHost = 'https://mycommunity.com/';
 var testRoot = path.resolve(__dirname) + '/..';
-var rewire = require('rewire');
 var spawn = require('child_process').spawn;
-var server;
+var rewire = require('rewire');
+var badErrorRespnse = 'Bad response';
+var successVersion = 15.7;
+var higherVersion = '15.7.1';
+var higherVersion2 = 16.1;
+var lowerVersion = 12.1;
 
 describe('test version check', function() {
   this.slow(1000);
@@ -32,55 +35,114 @@ describe('test version check', function() {
           versionNum + '-release r266968)</body></html>');
   }
 
+  function createServerMock(serverConfig) {
+    var serverApi = {};
+    Object.keys(serverConfig).forEach(function (key) {
+      serverApi[key] = function () {
+        return serverConfig[key];
+      };
+    });
+
+    serverApi['pluginUploadProtocol'] = function() {
+      var serverUrl = serverApi['serverUrl']();
+      if (serverUrl && serverUrl.indexOf('http://') > -1) {
+        return 'http';
+      }
+
+      return 'https';
+    };
+
+    return serverApi;
+  }
+
+  function createDefaultServerMock() {
+    return createServerMock({
+      //community: '',
+      dryRun: false,
+      force: false,
+      pluginPoints: [],
+      pluginToken: 'c95a3357-baed-4f09-9596-86583189b33e',
+      serverUrl: 'https://mycommunity.com/',
+      strictMode: false,
+      verbose: false,
+      toolVersion: '1.0.0',
+      allowStudioOverrides: false
+    });
+  }
+
   describe('version check response', function() {
     var versioncheck;
     var sandbox;
-    var gulp;
+    var server;
 
-    function check( done, expects, versionNumber) {
+    function check(expects, versionNumber, opts) {
 
+      var taskError;
       if (expects.serverError) {
-        createErrorRequest('Bad response');
+        createErrorRequest(badErrorRespnse);
       } else if (expects.respondSuccess) {
         createResponse(versionNumber);
       }
-      var vs = versioncheck(gulp, gutil);
-      console.log(vs);
+      var gulp = {
+        task: function(name, required, fn) {
+        }
 
+      };
+      rewire(testRoot + '/../gulp/versioncheck.js')(gulp, gutil).process(server, opts.cb);
     }
 
     before(function() {
-      //sinon.mock(gulp);
-      //sinon.stub(gulp, "task" , function() { return; } );
-      sandbox = sinon.sandbox.create();
-      gulp = sinon.mock();
-      sinon.stub(gulp, "task", function() {});
-      versioncheck = rewire(testRoot + '/../gulp/versioncheck.js');
-      //serverMock.__set__("serverConfigPath", "mock.server.conf.json");
+      server = createDefaultServerMock();
     });
 
     beforeEach(function() {
       nock.cleanAll();
+
     });
 
-    afterEach(function() {
+    it('should return error response from server', function(done) {
+      check( { serverError: true }, 15.7,
+          { cb: function(err) {
+            expect(err.message).to.equal(badErrorRespnse);
+            done();
+          }
+      });
     });
 
-    after(function() {
-    });
-
-
-    it('should return error', function(done) {
-      check( done,
-          { serverError: true },
-          15.7 );
-      done();
-    });
     it('should return success', function(done) {
-      check( done,
-          { respondSuccess: true },
-          15.7 );
+      var cb = function(err) {
+        expect.fail(true, "Did not expect errors");
+      };
+      check({ respondSuccess: true }, successVersion, { cb: cb});
       done();
     });
+
+    it('should return error for lower version', function(done) {
+      check( { respondSuccess: true }, lowerVersion,
+          { cb: function(err) {
+            expect(err.message).to.equal("Supported minimum version on server is "+successVersion);
+            done();
+          }
+      });
+    });
+
+    it('should return success for higher version', function(done) {
+      check( { respondSuccess: true }, higherVersion,
+          { cb: function(err) {
+            expect.fail(true, "Should not return error");
+          }
+      });
+      done();
+    });
+
+    it('should return success for higher version with major.min.subminor', function(done) {
+      check( { respondSuccess: true }, higherVersion2,
+          { cb: function(err) {
+            expect.fail(true, "Should not return error");
+          }
+          });
+      done();
+    });
+
   });
 });
