f63a060d0f519673cb49384fb95b4cec1c693fa4
dougs <doug.schroeder@lithium.com>
2016-03-16 14:45:29 -0700

    LIA-47340 add changes based on code review

--- bin/cmd/serve-sass.js
+++ bin/cmd/serve-sass.js
@@ -7,6 +7,6 @@ module.exports = {
     require('../../lib/spawn-gulp')(['watch-res-sass'].concat(process.argv.splice(3)));
   },
   help: 'Compiles the sass files in your chosen responsive skin (run li responsive-options to choose your ' +
-  '\nresponsive skin) and serves them locally. ' +
+  '\nresponsive skin) and serves the compiled css locally. ' +
   '\nRun this command from the root directory of a project.'
 };
