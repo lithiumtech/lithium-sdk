e40cf50be2140b6165d3f47f8fee492ba0953b7d
megha.meshram <megha.meshram@lithium.com>
2016-04-18 09:30:39 -0700

    LIA-44841 Code review fixes

--- bin/cmd/create-skin.js
+++ bin/cmd/create-skin.js
@@ -8,7 +8,7 @@ module.exports = {
     run: function () {
         //Success call back function
         var cb = require('../../gulp/createskin')(gulp, gutil).createNewSkin;
-        
+
         //Check if we are creating skin on a sdk project
         if (!fs.existsSync('./server.conf.json')) {
             console.log(gutil.colors.red('Please run create-skin under your Lithium SDK project directory.' +
@@ -21,5 +21,6 @@ module.exports = {
             debugMode: gutil.env['debug']
         }, cb);
     },
-    help: 'Creates a new skin.'
+    help: 'Creates a new skin.' + gutil.colors.bold('\nOptions:') +
+    '\n--clearFalse:   Downloads the core plugins from server even when directory is already present. Defaults to false'
 };
