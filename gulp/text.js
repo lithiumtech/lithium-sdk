2251ab1245e99277c667def2c2405f4f08b2f4f7
Adam Ayres <magicaj@gmail.com>
2016-06-15 16:40:48 -0700

    Handle when no text properties glob set.

--- gulp/text.js
+++ gulp/text.js
@@ -6,10 +6,14 @@ var path = lazyReq('path');
 module.exports = function (gulp, gutil) {
   var text = require('../lib/text.js')(gulp, gutil);
 
-  gulp.task('text', function () {
-    var textPropPattern = gutil.env.ng.textProperties.map(function (dir) {
-      return path().join(dir, text.FILES_PATTERN);
-    });
-    return text.processText(textPropPattern, text.TEXT_PLUGIN_PATH);
+  gulp.task('text', function (cb) {
+    if (gutil.env.ng.textProperties) {
+      var textPropPattern = gutil.env.ng.textProperties.map(function (dir) {
+        return path().join(dir, text.FILES_PATTERN);
+      });
+      return text.processText(textPropPattern, text.TEXT_PLUGIN_PATH);
+    } else {
+      cb();
+    }
   });
 };
