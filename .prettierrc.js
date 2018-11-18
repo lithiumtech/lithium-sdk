70fa3f375bb13e5196ff26d57a1237f876018df8
dougs <doug.schroeder@lithium.com>
2018-11-18 13:50:45 -0800

    LIA-59714 make component definitions added to res/feature/component-definition.xml reloadable.

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
