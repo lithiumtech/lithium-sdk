a623a9c135fee97b7967e99997b6537b2a295fc0
Gayathri S <gayathri.s@khoros.com>
2022-06-15 11:41:00 +0530

    LIA-64531-do version check and allow rendering themes only after lia version 22.7

--- gulp/skins.js
+++ gulp/skins.js
@@ -5,6 +5,10 @@ module.exports = function (gulp, gutil) {
 
   gulp.task('skins', ['skins-vars-template', 'skins-compile']);
 
+  gulp.task('skins-version-check', function () {
+  	return skins.checkVersionForThemeSupport();
+  });
+
   gulp.task('skins-compile', function (cb) {
     skins.compile().then(cb);
   });
